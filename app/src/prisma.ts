c104e96925204664910513a73d765416cc0e0edc
Daksh <hello@dak.sh>
2024-12-22 19:55:16 +0530

    add redis config

--- app/src/prisma.ts
+++ app/src/prisma.ts
@@ -3,11 +3,12 @@ import { PrismaClient } from "@prisma/client";
 import { Redis } from "ioredis";
 
 export const prisma = new PrismaClient();
-export const redis = new Redis({
-  host: "localhost",
-  port: 6379,
-  maxRetriesPerRequest: null,
-});
+export const redis = new Redis(
+  process.env.REDIS_URL! || "redis://localhost:6379",
+  {
+    maxRetriesPerRequest: null,
+  }
+);
 
 export const specAgentLockKey = (subtaskId: string) =>
   `spec-agent-lock:${subtaskId}`;
